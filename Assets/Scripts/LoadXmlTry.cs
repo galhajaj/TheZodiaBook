9b66b7d83c7a0b5e65eef3fe4dd53596dd4925a1
galhajaj <galhajaj@gmail.com>
2016-12-21 10:36:06 +0200

    add basic ui

---
+++ Assets/Scripts/LoadXmlTry.cs
@@ -0,0 +1,29 @@
+﻿using UnityEngine;
+using System.Collections;
+using System.Xml;
+
+public class LoadXmlTry : MonoBehaviour 
+{
+
+	// Use this for initialization
+	void Start () 
+    {
+        TextAsset textAsset = (TextAsset) Resources.Load("Xmls/lala");  
+        if (textAsset == null)
+            Debug.Log("xml file is null...");
+        XmlDocument xmldoc = new XmlDocument ();
+        xmldoc.LoadXml ( textAsset.text );
+
+        XmlNodeList elemList = xmldoc.GetElementsByTagName("Event");
+        for (int i=0; i < elemList.Count; i++)
+        {   
+            Debug.Log(elemList[i].InnerXml);
+        }
+	}
+	
+	// Update is called once per frame
+	void Update () 
+    {
+	
+	}
+}
